b3b97b545689de036ebe9aefcdb5f701b469e316
Chybaniii <chiebanilhaj@gmail.com>
2026-05-09 10:14:20 -1200

    Implement palindrome number checker in C++

---
+++ Algorithms-Level-2/#11-Palindrome-Number.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int ReadPositiveNumbers(string message)
+{
+	int Number = 0;
+
+	do
+	{
+		cout << message << endl;
+		cin >> Number;
+
+	} while (Number <= 0);
+
+	return Number;
+}
+
+int ReverseNumber(int Number)
+{
+	int Number2 = 0, Remainder = 0;
+
+	while (Number > 0)
+	{
+		Remainder = Number % 10;
+		Number = Number / 10;
+		Number2 = Number2 * 10 + Remainder;
+	}
+
+	return Number2;
+}
+
+bool IsPalindromeNumber(int Number)
+{
+	return Number == ReverseNumber(Number);
+}
+
+int main()
+{
+	if (IsPalindromeNumber(ReadPositiveNumbers("Please enter a positive number ?")))
+		cout << "\nYes, it is a palindrome number.\n";
+	else 
+		cout << "\nNo, it is not a palindrome number.\n";
+	
+
+	return 0;
+}
+
