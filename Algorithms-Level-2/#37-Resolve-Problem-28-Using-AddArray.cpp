03cd4ca55e1db0e8a3934d71b310a60cdfa65f28
Chybaniii <chiebanilhaj@gmail.com>
2026-05-09 10:36:36 -1200

    Add C++ program to fill and copy arrays

---
+++ Algorithms-Level-2/#37-Resolve-Problem-28-Using-AddArray.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+	int random = 0;
+	random = rand() % (To - From + 1) + From;
+
+	return random;
+}
+
+int ReadNumber()
+{
+	int Num = 0;
+	do
+	{
+		cout << "\nEnter a number?\n";
+		cin >> Num;
+	} while (Num <= 0);
+
+	return Num;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int& arrLength)
+{
+	cout << "\nEnter how many numbers do you need?\n";
+	cin >> arrLength;
+
+	for (int i = 0; i < arrLength; i++)
+		arr[i] = RandomNumber(1, 100);
+
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+	for (int i = 0; i < arrLength; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << "\n";
+}
+void AddArrayElements(int Number, int arr[100], int& arrLength)
+{
+	//Its a new element so we need to add the length by 1.
+	arrLength++;
+	arr[arrLength - 1] = Number;
+}
+
+void CopyArrayUsingAddArrayElements(int arrSource[100], int arrDestinition[100], int arrLength, int& arrDestinitionLength)
+{
+	for (int i = 0; i < arrLength; i++)
+		AddArrayElements(arrSource[i], arrDestinition, arrDestinitionLength);
+
+
+
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr[100], arrLength = 0, arr2Length = 0;
+
+	FillArrayWithRandomNumbers(arr, arrLength);
+
+	int arr2[100];
+	CopyArrayUsingAddArrayElements(arr, arr2, arrLength, arr2Length);
+
+	cout << "\nArray 1 elements:\n";
+	PrintArray(arr, arrLength);
+
+	cout << "\nArray 2 elements after copy:\n";
+	PrintArray(arr2, arr2Length);
+
+
+
+	return 0;
+}
