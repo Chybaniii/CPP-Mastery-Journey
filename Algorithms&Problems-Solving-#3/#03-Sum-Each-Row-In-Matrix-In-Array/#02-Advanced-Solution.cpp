9fc4a3e4a2df18c198abae3bf35f2e0f9124ddce
Chybaniii <chiebanilhaj@gmail.com>
2026-05-12 11:33:10 -1200

    Update #02-Advanced-Solution.cpp

---
+++ Algorithms&Problems-Solving-#3/#03-Sum-Each-Row-In-Matrix-In-Array/#02-Advanced-Solution.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+	int random = 0;
+	random = rand() % (To - From + 1) + From;
+	return random;
+}
+
+void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
+{
+	for (int i = 0; i < Rows; i++)
+	{
+		for (int j = 0; j < Cols; j++)
+			arr[i][j] = RandomNumber(1, 100);
+	}
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+	for (int i = 0; i < Rows; i++)
+	{
+		for (int j = 0; j < Cols; j++)
+		{
+			cout << setw(3) << arr[i][j] << "     ";
+		}
+		cout << endl;
+	}
+
+}
+
+
+int RowSum(int arr[3][3], short RowNumber, short Cols)
+{
+	int Sum = 0;
+	for (short j = 0; j <= Cols - 1; j++)
+	{
+		Sum += arr[RowNumber][j];
+	}
+	return Sum;
+}
+
+void SumMatrixRowsInArray(int arr[3][3], int arrSum[3], short Rows, short Cols)
+{
+	for (int i = 0; i < Rows; i++)
+	{
+		arrSum[i] = RowSum(arr, i, Cols);
+	}
+}
+
+void PrintRowsEachArray(int arr[3], short Rows)
+{
+	cout << "\nThe following are the sum of each row in the matrix:\n";
+
+	for (short i = 0; i < Rows; i++)
+	{
+		cout << "Row " << i + 1 << " Sum = " << arr[i] << endl;
+	}
+
+}
+
+
+int main()
+{
+	//Seeds the random number generator in C++, called only once.
+	srand((unsigned)time(NULL));
+
+	int arr[3][3];
+	int arrSum[3];
+
+	FillMatrixWithRandomNumbers(arr, 3, 3);
+
+	cout << "\n The following is a 3x3 random matrix:\n";
+	PrintMatrix(arr, 3, 3);
+
+	SumMatrixRowsInArray(arr, arrSum, 3, 3);
+	PrintRowsEachArray(arrSum, 3);
+	
+	system("pause>0");
+
+	return 0;
+
+}
+
+  
